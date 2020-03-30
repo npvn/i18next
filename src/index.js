6d14a1d037b0e393edaeb1f8664abd76b4b61598
Phuc Nguyen <contact@phucnguyen.info>
2020-03-30 21:14:50 +0700

    Force default module to be exported as a named key

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import i18next from './i18next.js';
 
+// Forcing the default export to be added as a `default` key, allowing the module
+// to be imported using ESM syntax in projects having `esModuleInterop` disabled.
+export const forceDefaultExportedAsNamedModule = true;
+
 export default i18next;
